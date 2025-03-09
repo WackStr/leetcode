39b0a7b797e9e7cfc253ea16e8639765b706c1c3
WackStr <waqasrazajafri@gmail.com>
2025-03-09 23:03:24 +0500

    Duplicate Zeros in array

---
+++ src/main/java/com/wackstr/leetcode/array_learn/DuplicateZeroes.java
@@ -0,0 +1,33 @@
+package com.wackstr.leetcode.array_learn;
+
+import java.util.Arrays;
+
+public class DuplicateZeroes {
+    public void duplicateZeros(int[] arr){
+        int n = arr.length;
+        int[] newArr = new int[n];
+        int newIndex = 0;
+        int oldIndex = 0;
+        while(newIndex < n){
+            newArr[newIndex] = arr[oldIndex];
+            if(arr[oldIndex] == 0){
+                newIndex++;
+                if(newIndex < n){
+                    newArr[newIndex] = 0;
+                }
+            }
+            oldIndex++;
+            newIndex++;
+        }
+        System.arraycopy(newArr, 0, arr, 0, n);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,0,2,3,0,4,5,0};
+        DuplicateZeroes sol = new DuplicateZeroes();
+        sol.duplicateZeros(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+}
