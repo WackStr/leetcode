89d185b81a7d49415f9f395aa2cc1a6e06f2afde
WackStr <waqasrazajafri@gmail.com>
2025-03-10 22:38:14 +0500

    2025-03-10 3306

---
+++ src/main/java/com/wackstr/leetcode/CountOfSubstrings_3306.java
@@ -0,0 +1,73 @@
+package com.wackstr.leetcode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+2025-03-10
+ */
+public class CountOfSubstrings_3306 {
+
+    public long countOfSubstrings(String word, int k) {
+        return atLeastK(word, k) - atLeastK(word, k + 1);
+    }
+
+    private long atLeastK(String word, int k) {
+        long numValidSubstrings = 0;
+        int start = 0;
+        int end = 0;
+        // keep track of counts of vowels and consonants
+        HashMap<Character, Integer> vowelCount = new HashMap<>();
+        int consonantCount = 0;
+
+        // start sliding window
+        while (end < word.length()) {
+            // insert new letter
+            char newLetter = word.charAt(end);
+
+            // update counts
+            if (isVowel(newLetter)) {
+                vowelCount.put(
+                        newLetter,
+                        vowelCount.getOrDefault(newLetter, 0) + 1
+                );
+            } else {
+                consonantCount++;
+            }
+
+            // shrink window while we have a valid substring
+            while (vowelCount.size() == 5 && consonantCount >= k) {
+                numValidSubstrings += word.length() - end;
+                char startLetter = word.charAt(start);
+                if (isVowel(startLetter)) {
+                    vowelCount.put(
+                            startLetter,
+                            vowelCount.get(startLetter) - 1
+                    );
+                    if (vowelCount.get(startLetter) == 0) {
+                        vowelCount.remove(startLetter);
+                    }
+                } else {
+                    consonantCount--;
+                }
+                start++;
+            }
+
+            end++;
+        }
+
+        return numValidSubstrings;
+    }
+
+    private boolean isVowel(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    public static void main(String[] args) {
+        String word = "iqeaouqi";
+        int k = 2;
+        CountOfSubstrings_3306 sol = new CountOfSubstrings_3306();
+        System.out.println(sol.countOfSubstrings(word, k));
+    }
+}
---
+++ src/main/java/com/wackstr/leetcode/array_learn/MergeArray.java
@@ -0,0 +1,39 @@
+package com.wackstr.leetcode.array_learn;
+
+import java.util.Arrays;
+
+public class MergeArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int index1 = m - 1;
+        int index2 = n - 1;
+        int currIndex = (m + n) - 1;
+        while(index1 >= 0 || index2 >= 0){
+            if(index1 >= 0 && index2 >= 0){
+                if(nums1[index1] > nums2[index2]){
+                    nums1[currIndex] = nums1[index1];
+                    index1--;
+                }else{
+                    nums1[currIndex] = nums2[index2];
+                    index2--;
+                }
+            }else if(index1 >= 0){
+                nums1[currIndex] = nums1[index1];
+                index1--;
+            }else{
+                nums1[currIndex] = nums2[index2];
+                index2--;
+            }
+            currIndex--;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int m = 3;
+        int[] nums2 = {2, 5, 6};
+        int n = 3;
+        MergeArray sol = new MergeArray();
+        sol.merge(nums1, m, nums2, n);
+        System.out.println(Arrays.toString(nums1));
+    }
+}
