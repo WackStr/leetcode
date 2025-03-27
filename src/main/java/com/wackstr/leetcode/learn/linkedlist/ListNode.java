c3027641eb8e784533387133f12c4196b70a7450
WackStr <waqasrazajafri@gmail.com>
2025-03-28 01:54:55 +0500

    LinkedLists

--- src/main/java/com/wackstr/leetcode/learn/linkedlist/ListNode.java
+++ src/main/java/com/wackstr/leetcode/learn/linkedlist/ListNode.java
@@ -1,12 +1,33 @@
 package com.wackstr.leetcode.learn.linkedlist;
 
 public class ListNode {
-    int val;
-    ListNode next;
+    public int val;
+    public ListNode next;
 
     public ListNode(int x){
         val = x;
         next = null;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+        ListNode curr = this;
+        while(curr != null){
+            s.append(String.format("%s -> ", curr.val));
+            curr = curr.next;
+        }
+        return s.toString();
+    }
+
+    public static ListNode fromArray(int[] nums){
+        ListNode head = new ListNode(nums[0]);
+        ListNode curr = head;
+        for(int i = 1; i < nums.length; i++){
+            curr.next = new ListNode(nums[i]);
+            curr = curr.next;
+        }
+        return head;
     }
 
 }
---
+++ src/main/java/com/wackstr/leetcode/learn/linkedlist/RemoveElement.java
@@ -0,0 +1,29 @@
+package com.wackstr.leetcode.learn.linkedlist;
+
+public class RemoveElement {
+    public ListNode removeElements(ListNode head, int val){
+        ListNode curr = head;
+        while(curr != null && curr.val == val){
+            curr = curr.next;
+        }
+        ListNode newHead = curr;
+        while(curr != null){
+            ListNode nextPtr = curr.next;
+            while(nextPtr != null && nextPtr.val == val){
+                nextPtr = nextPtr.next;
+            }
+            curr.next = nextPtr;
+            curr = nextPtr;
+        }
+        return newHead;
+    }
+
+    public static void main(String[] args) {
+        int[] listArray = {1,2,6,3,4,5,6};
+        int val = 6;
+        ListNode head = ListNode.fromArray(listArray);
+        RemoveElement sol = new RemoveElement();
+        System.out.println(head);
+        System.out.println(sol.removeElements(head, val));
+    }
+}
