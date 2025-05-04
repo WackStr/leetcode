f7c642f0bf3ecf9c6cc624f3fa757555830cd7e1
WackStr <waqasrazajafri@gmail.com>
2025-05-04 10:33:53 +0500

    2025-05-04 1128

--- src/main/java/com/wackstr/leetcode/EquivalentDominoes_1128.java
+++ src/main/java/com/wackstr/leetcode/EquivalentDominoes_1128.java
@@ -6,15 +6,13 @@ package com.wackstr.leetcode;
 public class EquivalentDominoes_1128 {
     public int numEquivDominoPairs(int[][] dominoes) {
         int[] count = new int[100];
+        int res = 0;
         for(int[] domino : dominoes){
             int index = domino[0] < domino[1] ?
                     domino[1] * 10 + domino[0] :
                     domino[0] * 10 + domino[1];
+            res += count[index];
             count[index]++;
-        }
-        int res = 0;
-        for(int n : count){
-            res += n * (n-1)/2;
         }
         return res;
     }
