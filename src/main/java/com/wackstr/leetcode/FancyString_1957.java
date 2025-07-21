a91466f90447729e0a0bea3e68c3464c6c95bd1a
WackStr <waqasrazajafri@gmail.com>
2025-07-21 11:21:14 +0500

    2025-07-21 1957

---
+++ src/main/java/com/wackstr/leetcode/FancyString_1957.java
@@ -0,0 +1,26 @@
+package com.wackstr.leetcode;
+
+/*
+2025-07-21
+ */
+public class FancyString_1957 {
+    public String makeFancyString(String s) {
+        char[] chars = s.toCharArray();
+        char last = chars[0];
+        int count = 1;
+        int pos = 1;
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] != last) {
+                last = chars[i];
+                count = 0;
+            }
+
+            if (++count > 2) continue;
+
+            chars[pos++] = chars[i];
+        }
+
+        return new String(chars, 0, pos);
+    }
+}
