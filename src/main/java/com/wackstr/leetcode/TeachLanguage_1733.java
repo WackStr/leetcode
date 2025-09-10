b7df5aaa0e4784829ee811eecac35c52ddc2215c
WackStr <waqasrazajafri@gmail.com>
2025-09-10 16:40:22 +0500

    2025-09-10 1733

---
+++ src/main/java/com/wackstr/leetcode/TeachLanguage_1733.java
@@ -0,0 +1,46 @@
+package com.wackstr.leetcode;
+
+import java.util.*;
+
+/*
+2025-09-10
+*/
+public class TeachLanguage_1733 {
+    public int minimumTeachings(int n, int[][] languages, int[][] friendships) {
+        Set<Integer> cncon = new HashSet<>();
+        for (int[] friendship : friendships) {
+            Map<Integer, Integer> mp = new HashMap<>();
+            boolean conm = false;
+            for (int lan : languages[friendship[0] - 1]) {
+                mp.put(lan, 1);
+            }
+            for (int lan : languages[friendship[1] - 1]) {
+                if (mp.containsKey(lan)) {
+                    conm = true;
+                    break;
+                }
+            }
+            if (!conm) {
+                cncon.add(friendship[0] - 1);
+                cncon.add(friendship[1] - 1);
+            }
+        }
+        int max_cnt = 0;
+        int[] cnt = new int[n + 1];
+        for (int friendship : cncon) {
+            for (int lan : languages[friendship]) {
+                cnt[lan]++;
+                max_cnt = Math.max(max_cnt, cnt[lan]);
+            }
+        }
+        return cncon.size() - max_cnt;
+    }
+
+    public static void main(String[] args) {
+        int n = 2;
+        int[][] languages = {{1}, {2}, {1,2}};
+        int[][] friendships = {{1,2}, {1,3}, {2,3}};
+        TeachLanguage_1733 sol = new TeachLanguage_1733();
+        System.out.println(sol.minimumTeachings(n, languages, friendships));
+    }
+}
