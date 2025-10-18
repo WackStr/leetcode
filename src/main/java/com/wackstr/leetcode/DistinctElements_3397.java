58bc5c7f573a4f8f047a796f6de03b88ee9157d9
WackStr <waqasrazajafri@gmail.com>
2025-10-18 22:45:42 +0500

    2025-10-18

---
+++ src/main/java/com/wackstr/leetcode/DistinctElements_3397.java
@@ -0,0 +1,22 @@
+package com.wackstr.leetcode;
+
+import java.util.Arrays;
+
+/*
+2025-10-18
+ */
+public class DistinctElements_3397 {
+    public int maxDistinctElements(int[] nums, int k) {
+        Arrays.sort(nums);
+        int cnt = 0;
+        int prev = Integer.MIN_VALUE;
+        for (int num : nums) {
+            int curr = Math.min(Math.max(num - k, prev + 1), num + k);
+            if (curr > prev) {
+                cnt++;
+                prev = curr;
+            }
+        }
+        return cnt;
+    }
+}
