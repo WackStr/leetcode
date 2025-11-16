140c1c82835e63f381c8c01820489db69c0937d0
WackStr <waqasrazajafri@gmail.com>
2025-11-16 10:07:24 +0500

    2025-11-16 1513

--- src/main/java/com/wackstr/leetcode/BinaryStringCount_1513.java
+++ src/main/java/com/wackstr/leetcode/BinaryStringCount_1513.java
@@ -6,20 +6,15 @@ package com.wackstr.leetcode;
 public class BinaryStringCount_1513 {
     
     private static final int MOD = 1_000_000_007;
-    
+
     public int numSub(String s){
-        int n = s.length();
+        int onesStreak = 0 ;
         int res = 0;
-        int left = 0;
-        int right;
-        while (left < n) {
-            while(left < n && s.charAt(left) == '0') left++;
-            right = left;
-            while(right < n && s.charAt(right) == '1') {
-                right++;
-                res = (res + (right - left)) % MOD;
+        for(char ch : s.toCharArray()) {
+            if (ch  == '1') {
+                res = ( res + ++onesStreak) % MOD ;
             }
-            left = right;
+            else    onesStreak = 0 ;
         }
         return res;
     }
