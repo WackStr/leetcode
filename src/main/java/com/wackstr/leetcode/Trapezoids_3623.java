3dc94746d20b04616e59f86ced94e50ebcc19971
WackStr <waqasrazajafri@gmail.com>
2025-12-02 19:38:43 +0500

    2025-12-02 3623

---
+++ src/main/java/com/wackstr/leetcode/Trapezoids_3623.java
@@ -0,0 +1,25 @@
+package com.wackstr.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+2025-12-2
+ */
+public class Trapezoids_3623 {
+    public int countTrapezoids(int[][] points) {
+        Map<Integer, Integer> pointNum = new HashMap<>();
+        final int mod = 1000000007;
+        long ans = 0;
+        long sum = 0;
+        for (int[] point : points) {
+            pointNum.put(point[1], pointNum.getOrDefault(point[1], 0) + 1);
+        }
+        for (int pNum : pointNum.values()) {
+            long edge = ((long) pNum * (pNum - 1)) / 2;
+            ans = (ans + edge * sum) % mod;
+            sum = (sum + edge) % mod;
+        }
+        return (int) ans;
+    }
+}
