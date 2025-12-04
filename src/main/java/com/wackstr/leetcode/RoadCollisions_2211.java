caa4c3bd0c8a621e1c5a20b2f62bafab551b9c9a
WackStr <waqasrazajafri@gmail.com>
2025-12-04 23:03:03 +0500

    2025-12-4 2211

---
+++ src/main/java/com/wackstr/leetcode/RoadCollisions_2211.java
@@ -0,0 +1,28 @@
+package com.wackstr.leetcode;
+
+/*
+2025-12-04
+ */
+public class RoadCollisions_2211 {
+    public int countCollisions(String directions) {
+        int n = directions.length();
+        int l = 0;
+        int r = n - 1;
+
+        while (l < n && directions.charAt(l) == 'L') {
+            l++;
+        }
+
+        while (r >= l && directions.charAt(r) == 'R') {
+            r--;
+        }
+
+        int res = 0;
+        for (int i = l; i <= r; i++) {
+            if (directions.charAt(i) != 'S') {
+                res++;
+            }
+        }
+        return res;
+    }
+}
