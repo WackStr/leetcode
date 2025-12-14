46ad3d9e9bc7efb161e7d84a8191c1bd03b4797a
WackStr <waqasrazajafri@gmail.com>
2025-12-14 11:13:15 +0500

    2025-12-14 2147

---
+++ src/main/java/com/wackstr/leetcode/DivideCorridor_2147.java
@@ -0,0 +1,26 @@
+package com.wackstr.leetcode;
+
+/*
+2025-12-14 2147
+ */
+public class DivideCorridor_2147 {
+    public int numberOfWays(String corridor) {
+        final int MOD = 1000_000_007;
+        long result = 1;
+        int prevSeatIndex = 0;
+        int numSeats = 0;
+
+        for(int i = 0; i < corridor.length(); i++){
+            char c = corridor.charAt(i);
+            if(c == 'S'){
+                numSeats++;
+                if(numSeats > 2 && numSeats % 2 == 1){
+                    result = result * (i - prevSeatIndex)%MOD;
+                }
+                prevSeatIndex = i;
+            }
+        }
+
+        return numSeats > 1 && numSeats % 2 == 0 ? (int)result : 0;
+    }
+}
