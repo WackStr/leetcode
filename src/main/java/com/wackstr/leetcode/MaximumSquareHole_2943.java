60fd34d0cefac8d278eb2015a015b763faf1800f
WackStr <waqasrazajafri@gmail.com>
2026-01-15 16:22:57 +0500

    2026-01-15 2943

---
+++ src/main/java/com/wackstr/leetcode/MaximumSquareHole_2943.java
@@ -0,0 +1,36 @@
+package com.wackstr.leetcode;
+
+import java.util.Arrays;
+
+/*
+2026-01-15 2943
+ */
+public class MaximumSquareHole_2943 {
+
+    public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+        Arrays.sort(hBars);
+        Arrays.sort(vBars);
+        int hmax = 1;
+        int vmax = 1;
+        int hcur = 1;
+        int vcur = 1;
+        for (int i = 1; i < hBars.length; i++) {
+            if (hBars[i] == hBars[i - 1] + 1) {
+                hcur++;
+            } else {
+                hcur = 1;
+            }
+            hmax = Math.max(hmax, hcur);
+        }
+        for (int i = 1; i < vBars.length; i++) {
+            if (vBars[i] == vBars[i - 1] + 1) {
+                vcur++;
+            } else {
+                vcur = 1;
+            }
+            vmax = Math.max(vmax, vcur);
+        }
+        int side = Math.min(hmax, vmax) + 1;
+        return side * side;
+    }
+}
