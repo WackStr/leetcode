37536e97650fb2a183c6560001cf94d634be6cdc
WackStr <waqasrazajafri@gmail.com>
2026-02-12 09:54:20 +0500

    2026-02-12 3713

---
+++ src/main/java/com/wackstr/leetcode/BalancedSubstring_3713.java
@@ -0,0 +1,35 @@
+package com.wackstr.leetcode;
+
+/*
+2026-02-12 3713
+ */
+public class BalancedSubstring_3713 {
+    public int longestBalanced(String s) {
+        char[] array = s.toCharArray();
+        int[] freq = null;
+        int n = array.length;
+        int ans = 1;
+        for (int i = 0; i < n - 1; i++) {
+            freq = new int[128];
+            int distinct = 0;
+            int maxFreq = 1;
+            for (int j = i; j < n; j++) {
+                freq[array[j]]++;
+                if (freq[array[j]] == 1) {
+                    distinct++;
+                }
+                maxFreq = Math.max(maxFreq, freq[array[j]]);
+                if ((j - i + 1) == maxFreq * distinct) {
+                    ans = Math.max(ans, j - i + 1);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        String s = "zzabccy";
+        BalancedSubstring_3713 sol = new BalancedSubstring_3713();
+        System.out.println(sol.longestBalanced(s));
+    }
+}
