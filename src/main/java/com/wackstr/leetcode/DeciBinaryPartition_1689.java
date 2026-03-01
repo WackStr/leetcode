2ddc40ff9908b9e55271a6121c9aa0f3fd7e69a1
WackStr <waqasrazajafri@gmail.com>
2026-03-01 16:30:05 +0500

    2026-03-01 1689

---
+++ src/main/java/com/wackstr/leetcode/DeciBinaryPartition_1689.java
@@ -0,0 +1,13 @@
+package com.wackstr.leetcode;
+
+/*
+2026-03-01 1689
+ */
+public class DeciBinaryPartition_1689 {
+    public int minPartitions(String n){
+        int res = 0;
+        for(int i = 0; i < n.length(); i++)
+            res = Math.max(res, n.charAt(i) - '0');
+        return res;
+    }
+}
