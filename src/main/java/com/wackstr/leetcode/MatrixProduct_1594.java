4a0bb129653a522f4dde610bce6b09db2ea7a968
WackStr <waqasrazajafri@gmail.com>
2026-03-23 12:52:46 +0500

    2026-03-23 1594

---
+++ src/main/java/com/wackstr/leetcode/MatrixProduct_1594.java
@@ -0,0 +1,50 @@
+package com.wackstr.leetcode;
+
+/*
+2026-03-23 1594
+ */
+public class MatrixProduct_1594 {
+
+    public int maxProductPath(int[][] grid) {
+        final int MOD = 1000000000 + 7;
+        int m = grid.length;
+        int n = grid[0].length;
+        long[][] maxgt = new long[m][n];
+        long[][] minlt = new long[m][n];
+
+        maxgt[0][0] = minlt[0][0] = grid[0][0];
+        for (int i = 1; i < n; i++) {
+            maxgt[0][i] = minlt[0][i] = maxgt[0][i - 1] * grid[0][i];
+        }
+        for (int i = 1; i < m; i++) {
+            maxgt[i][0] = minlt[i][0] = maxgt[i - 1][0] * grid[i][0];
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (grid[i][j] >= 0) {
+                    maxgt[i][j] =
+                            Math.max(maxgt[i][j - 1], maxgt[i - 1][j]) * grid[i][j];
+                    minlt[i][j] =
+                            Math.min(minlt[i][j - 1], minlt[i - 1][j]) * grid[i][j];
+                } else {
+                    maxgt[i][j] =
+                            Math.min(minlt[i][j - 1], minlt[i - 1][j]) * grid[i][j];
+                    minlt[i][j] =
+                            Math.max(maxgt[i][j - 1], maxgt[i - 1][j]) * grid[i][j];
+                }
+            }
+        }
+        if (maxgt[m - 1][n - 1] < 0) {
+            return -1;
+        } else {
+            return (int) (maxgt[m - 1][n - 1] % MOD);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {{1,3},{0,-4}};
+        MatrixProduct_1594 sol = new MatrixProduct_1594();
+        System.out.println(sol.maxProductPath(grid));
+    }
+}
