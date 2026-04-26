701a815ee3d5e9ce5d6a3a8fa89b675db212c98d
WackStr <waqasrazajafri@gmail.com>
2026-04-26 21:02:49 +0500

    2026-04-26 1559

--- src/main/java/com/wackstr/leetcode/Cycles2DGrid_1559.java
+++ src/main/java/com/wackstr/leetcode/Cycles2DGrid_1559.java
@@ -1,55 +1,72 @@
 package com.wackstr.leetcode;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Arrays;
 
 /*
 2026-04-26 1559
  */
 public class Cycles2DGrid_1559 {
-    private static final int[][] deltas = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-    public boolean containsCycle(char[][] grid){
-        boolean[][] visited = new boolean[grid.length][grid[0].length];
-        int[][] parents = new int[grid.length][grid[0].length];
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0; j < grid[0].length; j++){
-                if(!visited[i][j] && cycleExists(i, j, grid, visited, parents)) return true;
+
+    private static class UnionFind {
+
+        int[] parent;
+        int[] size;
+        int n;
+        int setCount;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            for (int i = 0; i < n; ++i) {
+                parent[i] = i;
             }
+            size = new int[n];
+            Arrays.fill(size, 1);
+            this.n = n;
+            setCount = n;
         }
-        return false;
+
+        public int findset(int x) {
+            return parent[x] == x ? x : (parent[x] = findset(parent[x]));
+        }
+
+        public void unite(int x, int y) {
+            if (size[x] < size[y]) {
+                int temp = x;
+                x = y;
+                y = temp;
+            }
+            parent[y] = x;
+            size[x] += size[y];
+            --setCount;
+        }
+
+        public boolean findAndUnite(int x, int y) {
+            int parentX = findset(x);
+            int parentY = findset(y);
+            if (parentX != parentY) {
+                unite(parentX, parentY);
+                return true;
+            }
+            return false;
+        }
     }
 
-    private boolean cycleExists(
-            int i, int j, char[][] grid, boolean[][] visited, int[][] parents) {
-        int n = grid.length;
-        int m = grid[0].length;
-        /*
-        BFS here, take care to not revisit parent
-         */
-        Queue<Integer> stack = new LinkedList<>();
-        stack.add(i*m + j);
-        while(!stack.isEmpty()){
-            int size = stack.size();
-            while(size > 0){
-                int curr = stack.poll();
-
-                int currX = curr / m;
-                int currY = curr % m;
-                char currVal = grid[currX][currY];
-                int parentX = parents[currX][currY] / m;
-                int parentY = parents[currX][currY] % m;
-                visited[currX][currY] = true;
-
-                for (int[] delta : deltas) {
-                    int newX = currX + delta[0];
-                    int newY = currY + delta[1];
-                    if(newX >= 0 && newX < n && newY >= 0 && newY < m && grid[newX][newY] == currVal && (newY != parentY || newX != parentX)){
-                        if(visited[newX][newY]) return true;
-                        parents[newX][newY] = curr;
-                        stack.add(newX * m + newY);
+    public boolean containsCycle(char[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        UnionFind uf = new UnionFind(m * n);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i > 0 && grid[i][j] == grid[i - 1][j]) {
+                    if (!uf.findAndUnite(i * n + j, (i - 1) * n + j)) {
+                        return true;
                     }
                 }
-                size--;
+                if (j > 0 && grid[i][j] == grid[i][j - 1]) {
+                    if (!uf.findAndUnite(i * n + j, i * n + j - 1)) {
+                        return true;
+                    }
+                }
             }
         }
         return false;
