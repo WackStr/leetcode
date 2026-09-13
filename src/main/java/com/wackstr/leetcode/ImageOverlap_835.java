bdd0cd3b4e5e021e15780fa40f5cd1825c5de9d7
WackStr <waqasrazajafri@gmail.com>
2026-09-13 14:09:50 +0500

    2026-09-13 835

---
+++ src/main/java/com/wackstr/leetcode/ImageOverlap_835.java
@@ -0,0 +1,32 @@
+package com.wackstr.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+2026-09-13 835
+ */
+public class ImageOverlap_835 {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+        int n = img1.length;
+        // collect every coordinate that holds a 1
+        List<int[]> A = new ArrayList<>();
+        List<int[]> B = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (img1[i][j] == 1) A.add(new int[]{i, j});
+                if (img2[i][j] == 1) B.add(new int[]{i, j});
+            }
+        }
+        int[][] cnt = new int[2 * n][2 * n];
+        int best = 0;
+        for (int[] a : A) {
+            for (int[] b : B) {
+                int dx = b[0] - a[0] + n;
+                int dy = b[1] - a[1] + n;
+                best = Math.max(best, ++cnt[dx][dy]);
+            }
+        }
+        return best;
+    }
+}
